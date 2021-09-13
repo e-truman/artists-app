79f0f01867ac58835624ec8c121c845cd989cf52
Erin <etruman2@gmail.com>
2021-09-13 10:28:20 -0500

    first commit with some starter code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Artists } from './components/Artists';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <BrowserRouter>
+        <Artists />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function

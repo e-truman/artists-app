1e991e01e8406dc39ec7573744beb0e45c09c743
Erin <etruman2@gmail.com>
2021-09-29 19:28:07 -0500

    tiny edits on artist dates module

--- src/components/artistDates/ArtistDates.js
+++ src/components/artistDates/ArtistDates.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { useHistory, useParams, Link } from "react-router-dom"
+import { Button } from 'reactstrap';
 import "./ArtistDates.css";
 
 
@@ -29,111 +30,113 @@ export const ArtistDates = (props) => {
 
     return (
         <>
-        <div className="artist-dates-container">
-            <div className="dates-background">
-                <h2 className="title">Music Venues and Live Performances</h2>
-                {
-                    artistDates.map((date) => {
+            <div className="artist-dates-container">
+                <div className="dates-background">
+                    <h2 className="title">Music Venues and Live Performances</h2>
+                    {
+                        artistDates.map((date) => {
 
-                        if (date.categoryId === 1) {
-                            return <>
+                            if (date.categoryId === 1) {
+                                return <>
 
-                                <ul>
-                                    <li>
-                                        <a className="date-link" href={date.link}>{date.title}</a>
-                                    </li>
-                                </ul>
-                            </>
-                        }
-                    })
-                }
-            </div>
-            <div className="dates-background">
-                <h2 className="title">Outdoors</h2>
-                {
-                    artistDates.map((date) => {
-                        if (date.categoryId === 1) {
-                            return <>
+                                    <ul>
+                                        <li>
+                                            <a className="date-link" href={date.link}>{date.title}</a>
+                                        </li>
+                                    </ul>
+                                </>
+                            }
+                        })
+                    }
+                </div>
+                <div className="dates-background">
+                    <h2 className="title">Outdoors</h2>
+                    {
+                        artistDates.map((date) => {
+                            if (date.categoryId === 2) {
+                                return <>
 
-                                <ul>
-                                    <li>
-                                        <a className="date-link" href={date.link}>{date.title}</a>
-                                    </li>
-                                </ul>
-                            </>
-                        }
-                    })
-                }
-            </div>
-            
-            <div className="dates-background">
-                <h2 className="title">Classes</h2>
-                {
-                    artistDates.map((date) => {
-                        if (date.categoryId === 3) {
-                            return <>
+                                    <ul>
+                                        <li>
+                                            <a className="date-link" href={date.link}>{date.title}</a>
+                                        </li>
+                                    </ul>
+                                </>
+                            }
+                        })
+                    }
+                </div>
 
-                                <ul>
-                                    <li>
-                                        <a className="date-link" href={date.link}>{date.title}</a>
-                                    </li>
-                                </ul>
-                            </>
-                        }
-                    })
-                }
-            </div>
-            <div className="dates-background">
-                <h2 className="title">Museums</h2>
-                {
-                    artistDates.map((date) => {
-                        if (date.categoryId === 4) {
-                            return <>
+                <div className="dates-background">
+                    <h2 className="title">Classes</h2>
+                    {
+                        artistDates.map((date) => {
+                            if (date.categoryId === 3) {
+                                return <>
 
-                                <ul>
-                                    <li>
-                                        <a className="date-link" href={date.link}>{date.title}</a>
-                                    </li>
-                                </ul>
-                            </>
-                        }
-                    })
-                }
-            </div>
-            <div className="dates-background">
-                <h2 className="title">Arts Stores</h2>
-                {
-                    artistDates.map((date) => {
-                        if (date.categoryId === 5) {
-                            return <>
+                                    <ul>
+                                        <li>
+                                            <a className="date-link" href={date.link}>{date.title}</a>
+                                        </li>
+                                    </ul>
+                                </>
+                            }
+                        })
+                    }
+                </div>
+                <div className="dates-background">
+                    <h2 className="title">Museums</h2>
+                    {
+                        artistDates.map((date) => {
+                            if (date.categoryId === 4) {
+                                return <>
 
-                                <ul>
-                                    <li>
-                                        <a className="date-link" href={date.link}>{date.title}</a>
-                                    </li>
-                                </ul>
-                            </>
-                        }
-                    })
-                }
-            </div>
-            <div className="dates-background">
-                <h2 className="title">Other</h2>
-                {
-                    artistDates.map((date) => {
-                        if (date.categoryId === 5) {
-                            return <>
+                                    <ul>
+                                        <li>
+                                            <a className="date-link" href={date.link}>{date.title}</a>
+                                        </li>
+                                    </ul>
+                                </>
+                            }
+                        })
+                    }
+                </div>
+                <div className="dates-background">
+                    <h2 className="title">Arts Stores</h2>
+                    {
+                        artistDates.map((date) => {
+                            if (date.categoryId === 6) {
+                                return <>
 
-                                <ul>
-                                    <li>
-                                        <a className="date-link" href={date.link}>{date.title}</a>
-                                    </li>
-                                </ul>
-                            </>
-                        }
-                    })
-                }
-            </div>
+                                    <ul>
+                                        <li>
+                                            <a className="date-link" href={date.link}>{date.title}</a>
+                                        </li>
+                                    </ul>
+                                </>
+                            }
+                        })
+                    }
+                </div>
+                <div className="dates-background">
+                    <h2 className="title">Other</h2>
+                    {
+                        artistDates.map((date) => {
+                            if (date.categoryId === 5) {
+                                return <>
+
+                                    <ul>
+                                        <li>
+                                            <a className="date-link" href={date.link}>{date.title}</a>
+                                        </li>
+                                    </ul>
+                                </>
+                            }
+                        })
+                    }
+                </div>
+                <Button className="buttons home-entries" onClick={() => history.push("/")}>
+                    HOME</Button>
             </div>
 
 
